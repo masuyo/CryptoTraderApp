06244f72be4789b5d76d566f58f5d5c8030e8679
Gabriella Sinka-Pálinkás <fusewent@gmail.com>
2019-04-11 02:21:56 +0200

    bleb

--- screens/UpcomingScreen.js
+++ screens/UpcomingScreen.js
@@ -3,6 +3,10 @@ import {StyleSheet, View} from 'react-native';
 import TouchableCounter from "../components/TouchableCounter";
 
 export default class UpcomingScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Upcoming shit',
+    };
+
     render() {
         return (
             <View style={styles.container}>
